eaf020d69209b7350a2576b7601a7e2b8b70a66e
booba-magistr <mainpo4ta1337@mail.ru>
2024-11-03 14:47:48 +0300

    calculator is done

--- static/js/test.js
+++ static/js/test.js
@@ -1,15 +1,31 @@
-let buttons = document.querySelectorAll('button')
-let total = document.querySelector('#total')
+let buttons = document.querySelectorAll('button');
+let total = document.querySelector('#total');
 
 buttons.forEach(element => {
     element.addEventListener('click', function() {
-        switch(total.textContent){
-            case '0':
-                total.textContent = element.textContent
-                break
+        switch(element.textContent){
+            case 'C':
+                total.textContent = '0';
+                break;
+            case '=':
+                total.textContent = eval(total.textContent)
+                break;
+            case '%':
+                let getPercent = total.textContent + '/100';
+                total.textContent = eval(getPercent);
+                break;
+            case '√':
+                let sqrt = total.textContent + '**0.5';
+                total.textContent = eval(sqrt);
+                break;
+            case '←':
+                let exp = total.textContent;
+                total.textContent = exp.substring(0, exp.length-1);
+                break;
             default:
-                total.textContent += element.textContent
-                break
+                if(total.textContent == '0' && element.textContent != '.')
+                    total.textContent = element.textContent;
+                else total.textContent += element.textContent;
         }
     })
-})  
+})
